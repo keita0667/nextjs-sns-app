6fb413457f3084fb238741ec41dc610c8d85fbf0
keita0667 <chorus1717@gmail.com>
2024-11-10 15:55:12 +0900

    不要なconsoleの削除

--- app/api/callback/clerk/route.ts
+++ app/api/callback/clerk/route.ts
@@ -4,7 +4,6 @@ import { WebhookEvent } from '@clerk/nextjs/server'
 import prisma from '@/lib/prisma'
 
 export async function POST(req: Request) {
-  console.log('ここを通った')
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
